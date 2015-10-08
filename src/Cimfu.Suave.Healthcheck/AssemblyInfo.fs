99ece5349fb7064e37ee51d4847884e30a4b2cd6
Marcus Griep <mgriep@cimpress.com>
2015-10-08 13:08:06 -0400

    Initial scaffolding

---
+++ src/Cimfu.Suave.Healthcheck/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Cimfu.Suave.Healthcheck")>]
+[<assembly: AssemblyProductAttribute("Cimfu.Suave.Healthcheck")>]
+[<assembly: AssemblyDescriptionAttribute("A pluggable healthcheck endpoint for the Suave functional web server.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
