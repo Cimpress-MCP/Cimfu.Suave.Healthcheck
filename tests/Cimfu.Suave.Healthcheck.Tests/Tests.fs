aa328eb83f90e7bc7e5884bd3bdbc2aa2fa66ef0
Marcus Griep <mgriep@cimpress.com>
2015-10-09 12:54:46 -0400

    Initial version of healthcheck feature

--- tests/Cimfu.Suave.Healthcheck.Tests/Tests.fs
+++ tests/Cimfu.Suave.Healthcheck.Tests/Tests.fs
@@ -2,9 +2,28 @@ module Cimfu.Suave.Healthcheck.Tests
 
 open Cimfu.Suave.Healthcheck
 open NUnit.Framework
+open Swensen.Unquote
+
+let testTimingSettings =
+  { GetTime = fun () -> NodaTime.Instant 0L
+    GetTimestamp = fun () -> 0L<stamp>
+    StampsPerSecond = 1L<stamp/sec> }
+
+let testEvaluateHealthcheck =
+  evaluateHealthcheckWith testTimingSettings
+
+let testEvaluateHealthchecks =
+  evaluateHealthchecksWith testTimingSettings testEvaluateHealthcheck
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``noop healthcheck returns success`` () =
+  let expected =
+    { GenerationTime = NodaTime.Instant 0L
+      Duration = NodaTime.Duration.Zero
+      Checks = Map.ofList
+        [ "noop", { TestedAt = NodaTime.Instant 0L
+                    Duration = NodaTime.Duration.Zero
+                    Result = HealthcheckResult.Healthy } ] }
+  let hcMap = Map.ofList ["noop", Checks.noop]
+  let actual = testEvaluateHealthchecks hcMap |> Async.RunSynchronously
+  actual =! expected
