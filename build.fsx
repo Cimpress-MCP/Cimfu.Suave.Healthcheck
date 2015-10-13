9c1b293da5682dc51355791e52a061df1ce2f977
Marcus Griep <mgriep@cimpress.com>
2015-10-13 01:10:03 -0400

    Revert "Switch to xUnit"
    This reverts commit 8049ae025cfc0e0b88ea3eb3ee00348da5043e58.

--- build.fsx
+++ build.fsx
@@ -7,7 +7,6 @@ open Fake
 open Fake.Git
 open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
-open Fake.Testing.XUnit2
 open System
 open System.IO
 #if MONO
@@ -44,7 +43,7 @@ let authors = [ "Marcus Griep" ]
 // Tags for your project (for NuGet package)
 let tags = "fsharp Suave healthcheck"
 
-// File system information
+// File system information 
 let solutionFile  = "Cimfu.Suave.Healthcheck.sln"
 
 // Pattern specifying assemblies to be tested using NUnit
@@ -52,7 +51,7 @@ let testAssemblies = "tests/**/bin/Release/*Tests*.dll"
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "Cimpress-MCP"
+let gitOwner = "Cimpress-MCP" 
 let gitHome = "git@github.com:" + gitOwner
 
 // The name of the project on GitHub
@@ -69,7 +68,7 @@ let gitRaw = environVarOrDefault "gitRaw" "https://raw.github.com/Cimpress-MCP"
 let release = LoadReleaseNotes "RELEASE_NOTES.md"
 
 // Helper active pattern for project types
-let (|Fsproj|Csproj|Vbproj|) (projFileName:string) =
+let (|Fsproj|Csproj|Vbproj|) (projFileName:string) = 
     match projFileName with
     | f when f.EndsWith("fsproj") -> Fsproj
     | f when f.EndsWith("csproj") -> Csproj
@@ -87,7 +86,7 @@ Target "AssemblyInfo" (fun _ ->
 
     let getProjectDetails projectPath =
         let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
-        ( projectPath,
+        ( projectPath, 
           projectName,
           System.IO.Path.GetDirectoryName(projectPath),
           (getAssemblyInfoAttributes projectName)
@@ -104,7 +103,7 @@ Target "AssemblyInfo" (fun _ ->
 )
 
 // Copies binaries from default VS location to exepcted bin folder
-// But keeps a subdirectory structure for each project in the
+// But keeps a subdirectory structure for each project in the 
 // src folder to support multiple project outputs
 Target "CopyBinaries" (fun _ ->
     !! "src/**/*.??proj"
@@ -137,13 +136,11 @@ Target "Build" (fun _ ->
 
 Target "RunTests" (fun _ ->
     !! testAssemblies
-    |> xUnit2 (fun p ->
+    |> NUnit (fun p ->
         { p with
-            ErrorLevel = Error
-            TimeOut = TimeSpan.FromMinutes 5.
-            Parallel = All
-            ShadowCopy = false
-            NUnitXmlOutputPath = Some "TestResults.xml" })
+            DisableShadowCopy = true
+            TimeOut = TimeSpan.FromMinutes 20.
+            OutputFile = "TestResults.xml" })
 )
 
 #if MONO
@@ -155,14 +152,14 @@ Target "RunTests" (fun _ ->
 Target "SourceLink" (fun _ ->
     let baseUrl = sprintf "%s/%s/{0}/%%var2%%" gitRaw (project.ToLower())
     use repo = new GitRepo(__SOURCE_DIRECTORY__)
-
-    let addAssemblyInfo (projFileName:String) =
+    
+    let addAssemblyInfo (projFileName:String) = 
        (projFileName, "**/*.g.*")
-
+        
     !! "src/**/*.??proj"
     |> Seq.map addAssemblyInfo
     |> Seq.iter (fun (projFile, assemblyInfo) ->
-        let proj = VsProj.LoadRelease projFile
+        let proj = VsProj.LoadRelease projFile 
         logfn "source linking %s" proj.OutputFilePdb
         let files = proj.Compiles -- assemblyInfo
         repo.VerifyChecksums files
@@ -178,7 +175,7 @@ Target "SourceLink" (fun _ ->
 // Build a NuGet package
 
 Target "NuGet" (fun _ ->
-    Paket.Pack(fun p ->
+    Paket.Pack(fun p -> 
         { p with
             OutputPath = "bin"
             Version = release.NugetVersion
@@ -186,7 +183,7 @@ Target "NuGet" (fun _ ->
 )
 
 Target "PublishNuget" (fun _ ->
-    Paket.Push(fun p ->
+    Paket.Push(fun p -> 
         { p with
             WorkingDir = "bin" })
 )
@@ -239,7 +236,7 @@ Target "GenerateHelpDebug" (fun _ ->
     generateHelp' true true
 )
 
-Target "KeepRunning" (fun _ ->
+Target "KeepRunning" (fun _ ->    
     use watcher = new FileSystemWatcher(DirectoryInfo("docs/content").FullName,"*.*")
     watcher.EnableRaisingEvents <- true
     watcher.Changed.Add(fun e -> generateHelp false)
@@ -259,7 +256,7 @@ Target "GenerateDocs" DoNothing
 
 let createIndexFsx lang =
     let content = """(*** hide ***)
-// This block of code is omitted in the generated HTML documentation. Use
+// This block of code is omitted in the generated HTML documentation. Use 
 // it to define helpers that you do not want to show in the documentation.
 #I "../../../bin"
 
@@ -321,11 +318,11 @@ Target "Release" (fun _ ->
 
     Branches.tag "" release.NugetVersion
     Branches.pushTag "" "origin" release.NugetVersion
-
+    
     // release on github
     createClientWithToken (getBuildParamOrDefault "github-token" "")
-    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
-    // TODO: |> uploadFile "PATH_TO_FILE"
+    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes 
+    // TODO: |> uploadFile "PATH_TO_FILE"    
     |> releaseDraft
     |> Async.RunSynchronously
 )
@@ -347,7 +344,7 @@ Target "All" DoNothing
   ==> "All"
   =?> ("ReleaseDocs",isLocalBuild)
 
-"All"
+"All" 
 #if MONO
 #else
   =?> ("SourceLink", Pdbstr.tryFind().IsSome )
@@ -365,7 +362,7 @@ Target "All" DoNothing
 
 "GenerateHelp"
   ==> "KeepRunning"
-
+    
 "ReleaseDocs"
   ==> "Release"
 
--- tests/Cimfu.Suave.Healthcheck.Tests/Tests.fs
+++ tests/Cimfu.Suave.Healthcheck.Tests/Tests.fs
@@ -1,42 +1,42 @@
 module Cimfu.Suave.Healthcheck.Tests
 
-open Xunit
+open NUnit.Framework
 open Swensen.Unquote
 
 open Cimfu.Suave.Healthcheck.Internal
 
-[<Fact>]
+[<Test>]
 let ``alwaysHealthy healthcheck returns Healthy`` () =
   let result = Checks.alwaysHealthy () |> Async.RunSynchronously
   result.Health =! Healthy
   result.Message =! None
 
-[<Fact>]
+[<Test>]
 let ``alwaysUnhealthy healthcheck returns Unhealthy`` () =
   let result = Checks.alwaysUnhealthy () |> Async.RunSynchronously
   result.Health =! Unhealthy
   result.Message =! Some "Service unavailable"
 
-[<Fact>]
+[<Test>]
 let ``serverMainSwitch is enabled by default`` () =
   let result = Checks.serverMain () |> Async.RunSynchronously
   result.Health =! Healthy
   result.Message =! Some "Server enabled"
 
-[<Fact>]
+[<Test>]
 let ``A switch is enabled by default`` () =
   let switch = HealthSwitch(testTimingSettings.GetTime, None, None)
   let result = switch.Check () |> Async.RunSynchronously
   result =! testDefaultHealthyData None
 
-[<Fact>]
+[<Test>]
 let ``Disabling a switch makes its healthcheck fail`` () =
   let switch = HealthSwitch(testTimingSettings.GetTime, None, None)
   switch.Disable ()
   let result = switch.Check () |> Async.RunSynchronously
   result =! testDefaultUnhealthyData None
 
-[<Fact>]
+[<Test>]
 let ``Disable/Reenable cycle on a switch acts as expected`` () =
   let switch = HealthSwitch(testTimingSettings.GetTime, None, None)
   switch.Disable ()
